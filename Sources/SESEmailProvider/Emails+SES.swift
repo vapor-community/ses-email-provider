12006df4df01f6bb0ad7c6e0fb2b851a1334b14e
Mads Odgaard <mads@madsodgaard.com>
2021-02-22 15:01:33 +0100

    Public extension

--- Sources/SESEmailProvider/Emails+SES.swift
+++ Sources/SESEmailProvider/Emails+SES.swift
@@ -2,8 +2,8 @@ import Vapor
 import SotoSES
 import Email
 
-extension Application.Emails.Provider {
-    public static func ses(client: AWSClient) -> Self {
+public extension Application.Emails.Provider {
+    static func ses(client: AWSClient) -> Self {
         .init {
             $0.emails.use {
                 SESEmailClientWrapper(ses: SES(client: client), eventLoop: $0.eventLoopGroup.next(), logger: $0.logger)
