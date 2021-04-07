979729764fa6e67a075490e551cea73c9efcaad8
Mads Odgaard <mads@madsodgaard.com>
2021-04-07 12:45:12 +0200

    Update to latest changes from vapor/email

--- Sources/SESEmailProvider/SESEmailClientWrapper.swift
+++ Sources/SESEmailProvider/SESEmailClientWrapper.swift
@@ -16,36 +16,50 @@ struct SESEmailClientWrapper: EmailClient {
     func send(_ messages: [EmailMessage]) -> EventLoopFuture<Void> {
         messages.map { message in
             let destination = SES.Destination(
-                bccAddresses: message.bcc?.map(\.description),
-                ccAddresses: message.cc?.map(\.description),
-                toAddresses: message.to.map(\.description))
+                bccAddresses: message.bcc?.map(\.fullAddress),
+                ccAddresses: message.cc?.map(\.fullAddress),
+                toAddresses: message.to.map(\.fullAddress)
+            )
             
             var htmlContent: SES.Content? = nil
             var textContent: SES.Content? = nil
             
-            if let html = message.content.html { htmlContent = .init(data: html) }
-            if let text = message.content.text { textContent = .init(data: text) }
+            switch message.content {
+            case let .text(text):
+                textContent = .init(data: text)
+            case let .html(html):
+                htmlContent = .init(data: html)
+            case let .universal(text, html):
+                textContent = .init(data: text)
+                htmlContent = .init(data: html)
+            }
             
-            let SESMessage = SES.Message(
+            let sesMessage = SES.Message(
                 body: .init(
                     html: htmlContent,
                     text: textContent
                 ),
-                subject: .init(data: message.subject))
+                subject: .init(data: message.subject)
+            )
             
             var replyTo: [String]? = nil
             if let _replyTo = message.replyTo {
                 replyTo = [_replyTo.email]
             }
             
-            let request = SES.SendEmailRequest(destination: destination, message: SESMessage, replyToAddresses: replyTo, source: message.from.description)
+            let request = SES.SendEmailRequest(
+                destination: destination,
+                message: sesMessage,
+                replyToAddresses: replyTo,
+                source: message.from.fullAddress
+            )
             return ses.sendEmail(request, logger: self.logger, on: eventLoop)
                 .transform(to: ())
         }
         .flatten(on: eventLoop)
     }
     
-    func delegating(to eventLoop: EventLoop) -> EmailClient {
+    func delegating(to eventLoop: EventLoop) -> SESEmailClientWrapper {
         SESEmailClientWrapper(ses: self.ses, eventLoop: eventLoop, logger: self.logger)
     }
 }
